e8826ecb505a226649ed857cfc966c0087c50815
Yang Qi <yangqi.engineer@gmail.com>
2023-11-22 17:47:04 +0800

    fix: 解决按键冲突问题

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -88,7 +88,8 @@ return {
         -- See `:help K` for why this keymap
         nmap("K", vim.lsp.buf.hover, "Hover Documentation")
         -- TODO: 按键冲突
-        nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+        -- nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+        nmap("gK", vim.lsp.buf.signature_help, "Signature Documentation")
 
         -- Lesser used LSP functionality
         nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
